3e5f7d9e5a256a74f5178e1b7c847836e9694ed9
Dowson <Dcheung@smail.pembinatrails.ca>
2022-03-18 12:54:55 -0500

    first commit

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,25 +1,26 @@
-import React, {useContext} from 'react';
-import {AppContext} from '../App';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 
 function Key({ keyVal, bigKey, disabled }) {
-  const { onDelete, onEnter, onSelectLetter } = useContext(AppContext);
-  //The "bigKey" prop will be a true or false statement to see if the "ENETER" and "DELETE" button will be bigger or not on the screen keyboard.
+  const { onSelectLetter, onDelete, onEnter } =
+    useContext(AppContext);
+
   const selectLetter = () => {
     if (keyVal === "ENTER") {
-      onEnter()
+      onEnter();
     } else if (keyVal === "DELETE") {
-      onDelete()
-    }else {
-    onSelectLetter(keyVal);
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
     }
   };
   return (
     <div className="key" id={bigKey ? "big" : disabled && "disabled"} onClick={selectLetter}>
+      {/*The id in the div box will be dependent on whether the key should be big or not.*/}
+      {/*I added an "onClick" method so when you click the letters it will be able to print it out on the grid/board because I made a function called "selectLetter".*/}
       {keyVal}
     </div>
-      /*I added an "onClick" method so when you click the letters it will be able to print it out on the grid/board because I made a function called "selectLetter".*/
-    //The id in the div box will be dependent on whether the key should be big or not.
   );
 }
- 
+
 export default Key;
